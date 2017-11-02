cdc3d917970f08cabff0a5ff4eb20c14be8ec925
lzzhanglin <zl950@foxmail.com>
2017-11-02 12:40:10 +0800

    20171102
    12:39

--- mxshop/settings.py
+++ mxshop/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,BASE_DIR)
+sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
+sys.path.insert(0,os.path.join(BASE_DIR,'extra_apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
+    'apps.users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -77,10 +81,15 @@ WSGI_APPLICATION = 'mxshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "mxshop",
+        'USER': 'root',
+        'PASSWORD': "zhanglin999",
+        'HOST': "127.0.0.1",
+        'OPTIONS': { 'init_command': 'SET default_storage_engine=INNODB;' }
     }
 }
+
 
 
 # Password validation
