0f62fb717897ffb2caf847a0606f3366950d2d1a
lzzhanglin <zl950@foxmail.com>
2017-11-06 13:16:57 +0800

    5-5

--- mxshop/urls.py
+++ mxshop/urls.py
@@ -13,16 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 # from django.contrib import admin
 import xadmin
 from mxshop.settings import MEDIA_ROOT
 from django.views.static import serve
-from goods.views_base import GoodsListView
+from rest_framework.documentation import include_docs_urls
+from goods.views import GoodsListView
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^media/(?P<path>.*)$',serve, {"document_root": MEDIA_ROOT}),
     url(r'goods/$', GoodsListView.as_view(),name="goods-list"),
-
+    url(r'docs/', include_docs_urls(title="慕学生鲜")),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
