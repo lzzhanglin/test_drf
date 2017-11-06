3508f0d1e2ddbb5de0e4d3d96d3edcb9f2b2e383
lzzhanglin <zl950@foxmail.com>
2017-11-06 13:16:41 +0800

    5-5

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Goods
+from .serializers import GoodsSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+class GoodsListView(APIView):
+    """
+    List all goods, or create a new goods.
+    """
+    def get(self, request, format=None):
+        goods = Goods.objects.all()[:10]
+        goods_serializers = GoodsSerializer(goods, many=True)
+        return Response(goods_serializers.data)
+
+    # def post(self, request, format=None):
+    #     serializer = GoodsSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
